bcbec6b62575131efc2397f0d0abb6bb2b797abb
iurii polnikov <iuriipolnikov@gmail.com>
2022-09-16 12:12:19 +0300

    DEBUG=False in main branch

--- rating/rating/settings.py
+++ rating/rating/settings.py
@@ -10,11 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-
-INTERNAL_IPS = ["127.0.0.1",]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fiegh-rating.ru', '188.225.79.76']
 
 INSTALLED_APPS = [
     'django.contrib.auth',
