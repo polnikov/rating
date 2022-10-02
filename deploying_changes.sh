3f911594724dbf0d2eea6131a57f75b6d7ca4e1c
iurii polnikov <iuriipolnikov@gmail.com>
2022-10-02 13:06:39 +0300

    update workflow

--- deploying_changes.sh
+++ deploying_changes.sh
@@ -10,6 +10,8 @@ echo "Git pull..."
 git pull
 
 cd /home/code
+echo "Target folder: $(pwd)"
+
 # check & activate env
 if [[ -d "env" ]]; then
     echo 'Venv exists...'
@@ -20,6 +22,8 @@ else
 fi
 
 cd /home/code/rating
+echo "Target folder: $(pwd)"
+
 sed -i "/psycopg2==2.9.3/d" requirements.txt
 
 # install requirements
@@ -28,6 +32,7 @@ pip install -r requirements.txt
 
 # cd to root project
 cd /home/code/rating/rating
+echo "Target folder: $(pwd)"
 
 # makemigrations and migrate
 echo "Make migrations and migrate..."
@@ -38,9 +43,12 @@ python manage.py migrate
 echo "Collect staticfiles..."
 manage.py collectstatic --noinput
 
+echo "Deactivate venv..."
 deactivate
 
 # restart gunicorn and reload nginx
 echo "Restart Gunicorn & NGINX..."
 sudo systemctl restart gunicorn
 sudo systemctl reload nginx
+
+echo "Finish!"
