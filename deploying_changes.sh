b3fb2492c20fb6740d5b95367c9ceef32b3faf47
iurii polnikov <iuriipolnikov@gmail.com>
2022-10-03 12:37:37 +0300

    update workflow

--- deploying_changes.sh
+++ deploying_changes.sh
@@ -1,5 +1,54 @@
+#!/bin/bash
+
+echo '[-------------] Start deploying changes...'
+
+cd /home/code/rating
+echo "[-------------] Target folder: $(pwd)"
+
+# pull the changes
+echo "[-------------] Git pull..."
+git pull
+
+cd /home/code
+echo "[-------------] Target folder: $(pwd)"
+
+# check & activate env
 if [ -d /home/code/env ]; then
-    echo '[-------------] Yes'
+    echo '[-------------] Venv exists!'
+    . ./env/bin/activate
 else
-    echo '[-------------] No'
+    echo '[-------------] Creating Venv...'
+    python3 -m venv env
 fi
+
+cd /home/code/rating
+echo "[-------------] Target folder: $(pwd)"
+
+sed -i "/psycopg2==2.9.3/d" requirements.txt
+
+# install requirements
+echo "[-------------] Install dependencies..."
+pip install -r requirements.txt
+
+# cd to root project
+cd /home/code/rating/rating
+echo "[-------------] Target folder: $(pwd)"
+
+# makemigrations and migrate
+echo "[-------------] Make migrations and migrate..."
+python manage.py makemigrations
+python manage.py migrate
+
+# collectstatic
+echo "[-------------] Collect staticfiles..."
+manage.py collectstatic --noinput
+
+echo "[-------------] Deactivate venv..."
+deactivate
+
+# restart gunicorn and reload nginx
+echo "[-------------] Restart Gunicorn & NGINX..."
+sudo systemctl restart gunicorn
+sudo systemctl reload nginx
+
+echo "[-------------] Finish!"
