eb161fb02ee8aa628ef5fe239b3cc0a5b9df90b9
iurii polnikov <iuriipolnikov@gmail.com>
2023-04-09 10:58:38 +0300

    add logger information

--- rating/api/urls.py
+++ rating/api/urls.py
@@ -19,17 +19,17 @@ router.register('semesters', views.SemesterViewSet)
 
 urlpatterns = [
     path('v1/', include(router.urls)),
-    path('v1/history/students/', views.StudentLogList.as_view()),
-    path('v1/history/subjects/', views.SubjectLogList.as_view()),
-    path('v1/groupmarks/', views.group_marks),
-    path('v1/money/', views.StudentMoneyList.as_view()),
-    path('v1/debts/students/', views.students_debts),
-    path('v1/rating/', views.student_rating),
-    path('v1/search/', views.search),
+    path('v1/history/students/', views.StudentLogList.as_view()),  # [-]
+    path('v1/history/subjects/', views.SubjectLogList.as_view()),  # [-]
+    path('v1/groupmarks/', views.group_marks), # [+]
+    path('v1/money/', views.StudentMoneyList.as_view()),  # [-]
+    path('v1/debts/students/', views.students_debts),  # [-]
+    path('v1/rating/', views.student_rating),  # [-]
+    path('v1/search/', views.search),  # [-]
 
-    path('v1/transferstudents/', views.transfer_students),
-    path('v1/import/students/', views.import_students),
-    path('v1/import/results/', views.import_results),
+    path('v1/transferstudents/', views.transfer_students),  # [-]
+    path('v1/import/students/', views.import_students),  # [-]
+    path('v1/import/results/', views.import_results),  # [-]
     # path('v1/import/subjects/', views.import_subjects),
 ]
 
